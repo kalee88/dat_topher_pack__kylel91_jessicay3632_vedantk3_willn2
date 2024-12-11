dbbc4ba3d31486197df4e2439d2797864d47d272
Vedant Kothari <vkothari50@stuy.edu>
2024-12-11 11:44:28 -0500

    Added navbar code with working links

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,13 +1,13 @@
 # Kyle Lee, Jessica Yu, Vedant Kothari, Will Nzeuton
 # Team datTopherPack
 # SoftDev
-# p01 
+# p01
 # 2024-12-07
 
 from flask import render_template, request, redirect, url_for, flash, session
 from app import app
 from .auth import *
-from .api import *
+## from .api import *
 
 @app.route("/")
 def home():
