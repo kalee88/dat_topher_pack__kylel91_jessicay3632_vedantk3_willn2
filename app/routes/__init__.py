ed3b50176aa98e9564ee9dc345a8e4f9332cd017
Kyle Lee <klee50@stuy.edu>
2024-12-11 11:21:38 -0500

    Added skeleton routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -7,7 +7,24 @@
 from flask import render_template, request, redirect, url_for, flash, session
 from app import app
 from .auth import *
+from .api import *
 
 @app.route("/")
 def home():
     return render_template('index.html', is_logged_in = auth.is_logged_in(), logged_in_user = auth.get_logged_in_user())
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
+@app.route("/arts")
+def arts():
+    return render_template("arts.html")
+
+@app.route("/sports")
+def sports():
+    return render_template("sports.html")
+
+@app.route("/weather")
+def weather():
+    return render_template("weather.html")
